a8a6f37df5bae1601130c69d2ac097aa7a080010
Saib Ahmed <saibahmed1512@gmail.com>
2018-09-08 09:24:23 +0530

    added signUp activity

---
+++ app/src/main/java/com/example/saibahmed/chitchat/RagisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.saibahmed.chitchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RagisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ragister);
+    }
+}
