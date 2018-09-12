51898adf8dd5a4d7a8506616b7df2af981404c80
Saib Ahmed <saibahmed1512@gmail.com>
2018-09-12 20:51:32 +0530

    commit

--- app/src/main/java/com/example/saibahmed/chitchat/LoginActivity.java
+++ app/src/main/java/com/example/saibahmed/chitchat/LoginActivity.java
@@ -1,21 +1,52 @@
 package com.example.saibahmed.chitchat;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Ref to firebase
+    private FirebaseAuth myAuth;
+
+    //UI refs
+    private EditText myEmail;
+    private EditText myPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Grab data
+        myEmail=findViewById(R.id.email_login);
+        myPassword=findViewById(R.id.pass_login);
+
+        //get firebase instance
+        myAuth=FirebaseAuth.getInstance();
     }
 
+    //move user to ragister activity
     public void ragisterNewUser(View v){
         Intent intent=new Intent(this,RagisterActivity.class);
         finish();
         startActivity(intent);
+    }
+
+
+    //create error box for errors
+    private void showErrorBox(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Heyyyy")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
--- app/src/main/java/com/example/saibahmed/chitchat/RagisterActivity.java
+++ app/src/main/java/com/example/saibahmed/chitchat/RagisterActivity.java
@@ -6,9 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -45,32 +48,85 @@ public class RagisterActivity extends AppCompatActivity {
         myAuth = FirebaseAuth.getInstance();
     }
 
-    //Use shared prefs for username
-    private void saveUserName(){
-        String userName = myUserNameView.getText().toString();
-        SharedPreferences pref = getSharedPreferences(CHAT_PREF ,0);
-        pref.edit().putString(DISPLAY_NAME,userName).apply();
+    //methods called by tapping
+    public void signUp(View v){
+        registerUser();
     }
 
+    //actual registration happens here
+    private void registerUser(){
+        myEmail.setError(null);
+        myPassword.setError(null);
+        myPasswordConfirm.setError(null);
+        myUserNameView.setError(null);
+
+        //grab values
+        String email = myEmail.getText().toString();
+        String password = myPassword.getText().toString();
+
+        boolean cancel= false;
+        View focusView = null;
+
+        //password validation
+        if (!TextUtils.isEmpty(password)  && !checkPassword(password)){
+            myPassword.setError(getString(R.string.invalid_password));
+            focusView = myPassword;
+            cancel = true;
+        }
+
+        //email validation
+        if (!TextUtils.isEmpty(email) && !checkEmail(email)){
+            myEmail.setError(getString(R.string.invalid_email));
+            focusView = myEmail;
+            cancel=true;
+        }
+
+        if (cancel){
+            focusView.requestFocus();
+        }else{
+            createUser();
+        }
+    }
+
+
+    //Use shared prefs for username
+    private void saveUserName() {
+        String userName = myUserNameView.getText().toString();
+        SharedPreferences pref = getSharedPreferences(CHAT_PREF, 0);
+        pref.edit().putString(DISPLAY_NAME, userName).apply();
+    }
+
+    //validation for email
+    private boolean checkEmail(String email){
+        return email.contains("@");
+    }
+
+    //validation for password
+    private boolean checkPassword(String password){
+        String confPassword=myPasswordConfirm.getText().toString();
+        return confPassword.equals(password) && password.length() > 4;
+    }
+
+
     //signUp user at firebase
-    private void createUser(){
+    private void createUser() {
         //Grab values
         String email = myEmail.getText().toString();
         String password = myPassword.getText().toString();
 
         //call method from firebase
-        myAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        myAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.i("FINDCODE","user creation was" +task.isSuccessful());
-                if (!task.isSuccessful()){
+                Log.i("FINDCODE", "user creation was" + task.isSuccessful());
+                if (!task.isSuccessful()) {
                     showErrorBox("oops ragistration failed");
-                }else{
+                } else {
                     saveUserName();
-                    //todo make a toast
+                    Toast.makeText(RagisterActivity.this, "Ragistration successful", Toast.LENGTH_SHORT).show();
 
                     //move user to login screen on success
-                    Intent intent = new Intent(RagisterActivity.this,LoginActivity.class);
+                    Intent intent = new Intent(RagisterActivity.this, LoginActivity.class);
                     finish();
                     startActivity(intent);
                 }
@@ -81,11 +137,11 @@ public class RagisterActivity extends AppCompatActivity {
     }
 
     //create error box for errors
-    private void showErrorBox(String message){
+    private void showErrorBox(String message) {
         new AlertDialog.Builder(this)
                 .setTitle("Heyyyy")
                 .setMessage(message)
-                .setPositiveButton(android.R.string.ok,null)
+                .setPositiveButton(android.R.string.ok, null)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
