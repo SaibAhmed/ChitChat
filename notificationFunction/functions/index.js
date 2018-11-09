c5407346dbbfe670f0e173cff9ee5573208d3e10
Saib Ahmed <saibahmed1512@gmail.com>
2018-11-09 15:10:39 +0530

    commit

--- notificationFunction/functions/index.js
+++ notificationFunction/functions/index.js
@@ -1,16 +1,9 @@
 'use strict'
-
 const functions = require('firebase-functions');
-const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-exports.sendNotification = functions.database.ref("/notifications/{user_id}/{notification_id}").onWrite(event => {
-    const user_id = event.params.user_id;
-    const notification_id = event.params.notification;
-    console.log("we have a notification to send to", user_id);
+exports.sendNotification = functions.database.ref('/notifications/{user_id}').onWrite((Change, context) => {
 
-    if (!event.data.val()) {
-        return console.log("A Notification has been deleted from database", notification_id);
-    }
-
+    console.log('Testing stuff', context.params.user_id)
 });
