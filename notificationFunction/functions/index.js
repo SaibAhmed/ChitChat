41b4d1dbbfff5ad4e43d8a2f7ae1812e19691bb7
Saib Ahmed <saibahmed1512@gmail.com>
2018-11-20 12:16:19 +0530

    updates

--- notificationFunction/functions/index.js
+++ notificationFunction/functions/index.js
@@ -1,9 +1,13 @@
 'use strict'
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.sendNotification = functions.database.ref('/notifications/{user_id}').onWrite((Change, context) => {
+exports.sendNotification = functions.database
+  .ref('/notifications/{user_id}')
+  .onWrite((change, context) => {
+    const user_id = context.params.user_id;
+    const notification = event.params.notification;
 
-    console.log('Testing stuff', context.params.user_id)
-});
+    console.log("the user id is :", user_id);
+  });
